1e41b2475d658a00d93d70fc7bbfee42521293b1
ZeTol <Linkedin12332@gmail.com>
2023-02-26 10:39:21 +0300

    ок

--- autotests_rostelecom.py
+++ autotests_rostelecom.py
@@ -4,8 +4,8 @@ from pages.auth_page import AuthPage
 from pages.registration_page import RegPage
 
 
-# Тест-кейс_1.
-# Корректное отображение "Стандартной страницы авторизации".
+# TC-LK.RT-1.
+# Загрузка формы разрешения входа.
 def test_start_page_is_correct(web_browser):
     page = AuthPage(web_browser)
     phone_tab_class = page.phone_tab.get_attribute("class")
@@ -18,7 +18,7 @@ def test_start_page_is_correct(web_browser):
     assert page.logo_lk.get_text() == "Личный кабинет"
 
 
-# Тест-кейс_2. Ошибка.
+# TC-LK.RT-2. Ошибка.
 # Проверка элементов в левом и правом блоке страницы.
 @pytest.mark.xfail(reason="Расположение элементов на странице не соответсвует ожидаемым требованиям")
 def test_location_of_page_blocks(web_browser):
@@ -27,7 +27,7 @@ def test_location_of_page_blocks(web_browser):
     assert page.lk_form.find(timeout=1)
 
 
-# Тест-кейс_3. Ошибка.
+# TC-LK.RT-3. Ошибка.
 # Проверка выбора «Номер».
 @pytest.mark.xfail(reason="Таб выбора 'Номер' не соответсвует ожидаемым требованиям")
 def test_phone_tab(web_browser):
@@ -35,7 +35,7 @@ def test_phone_tab(web_browser):
     assert page.phone_tab.get_text() == "Номер"
 
 
-# Тест-кейс_4. Ошибка.
+# TC-LK.RT-4. Ошибка.
 # Проверка название кнопки «Продолжить» в форме «Регестрация».
 @pytest.mark.xfail(reason="Кнопка должна иметь текст 'Продолжить'")
 def test_registration_page_and_continue_button(web_browser):
@@ -51,7 +51,7 @@ def test_registration_page_and_continue_button(web_browser):
     assert reg_page.continue_button.get_text() == "Продолжить"
 
 
-# Тест-кейс_5.
+# TC-LK.RT-5.
 # Регистрация пользователя с пустым полем «Имя», появления текста с подсказкой об ошибке.
 def test_registration_page_with_empty_name_field(web_browser):
     auth_page = AuthPage(web_browser)
@@ -67,7 +67,7 @@ def test_registration_page_with_empty_name_field(web_browser):
     assert reg_page.error_message_name.get_text() == "Необходимо заполнить поле кириллицей. От 2 до 30 символов."
 
 
-# Тест-кейс_6
+# TC-LK.RT-6
 # Регистрация пользователя с некорректным значением в поле «Имя»(менее 2-х символов), появление текста об ошибке.
 def test_registration_with_an_incorrect_value_in_the_name_field(web_browser):
     auth_page = AuthPage(web_browser)
@@ -83,7 +83,7 @@ def test_registration_with_an_incorrect_value_in_the_name_field(web_browser):
     assert reg_page.error_message_name.get_text() == "Необходимо заполнить поле кириллицей. От 2 до 30 символов."
 
 
-# Тест-кейс_7
+# TC-LK.RT-7
 # Регистрация пользователя с некорректным значением в поле «Фамилия» (более 30-ти символов), появление текста с ошибкой.
 def test_registration_with_an_incorrect_value_in_the_last_name_field(web_browser):
     auth_page = AuthPage(web_browser)
@@ -99,7 +99,7 @@ def test_registration_with_an_incorrect_value_in_the_last_name_field(web_browser
     assert reg_page.error_message_last_name.get_text() == "Необходимо заполнить поле кириллицей. От 2 до 30 символов."
 
 
-# Тест-кейс_8
+# TC-LK.RT-8
 # Регистрация пользователя с уже зарегистрированным номером, отображается оповещающая форма.
 def test_registration_of_an_already_registered_user(web_browser):
     auth_page = AuthPage(web_browser)
@@ -114,7 +114,7 @@ def test_registration_of_an_already_registered_user(web_browser):
     assert reg_page.notification_form.is_visible
 
 
-# Тест-кейс_9. Ошибка.
+# TC-LK.RT-9. Ошибка.
 # Проверка кнопки «х» - закрыть всплывающее окно оповещения.
 @pytest.mark.xfail(reason="Должна быть кнопка закрыть 'х'")
 def test_notification_form(web_browser):
@@ -132,7 +132,7 @@ def test_notification_form(web_browser):
     assert reg_page.close_button.get_text() == 'x'
 
 
-# Тест-кейс_10
+# TC-LK.RT-10
 # Некорректный пароль при регистрации пользователя(менее 8-ми символов), появления текста с подсказкой об ошибке.
 def test_incorrect_password_during_registration(web_browser):
     auth_page = AuthPage(web_browser)
@@ -148,7 +148,7 @@ def test_incorrect_password_during_registration(web_browser):
 
 
 
-# Тест-кейс_11
+# TC-LK.RT-11
 # Вход по неправильному паролю в форме «Авторизация» уже зарегистрированного пользователя, надпись «Забыл пароль» перекрашивается в оранжевый цвет.
 def test_authorization_of_a_user_with_an_invalid_password(web_browser):
     page = AuthPage(web_browser)
@@ -159,7 +159,7 @@ def test_authorization_of_a_user_with_an_invalid_password(web_browser):
     assert "rt-link--orange" in page.the_element_forgot_the_password.get_attribute('class')
 
 
-# Тест-кейс_12
+# TC-LK.RT-12
 # Регистрация пользователя в форме «Регистрации» в поле ввода «Фамилия» вместо кириллицы, недопустимые символы.
 def test_instead_of_cyrillic_invalid_characters(web_browser):
     auth_page = AuthPage(web_browser)
@@ -175,7 +175,7 @@ def test_instead_of_cyrillic_invalid_characters(web_browser):
 
 
 
-# Тест-кейс_13
+# ТC-LK.RT-13
 # Поле ввода «Пароль» и поле ввода «Подтверждение пароля»  в форме «Регистрация» не совпадают.
 def test_password_and_password_confirmation_do_not_match(web_browser):
     auth_page = AuthPage(web_browser)
@@ -190,7 +190,7 @@ def test_password_and_password_confirmation_do_not_match(web_browser):
     assert reg_page.message_passwords_dont_match.get_text() == "Пароли не совпадают"
 
 
-# Тест-кейс_14
+# ТC-LK.RT-14
 # Не валидный email в поле ввода «Email или мобильный телефон».
 def test_invalid_email_or_mobile_phone(web_browser):
     auth_page = AuthPage(web_browser)
@@ -206,7 +206,7 @@ def test_invalid_email_or_mobile_phone(web_browser):
                                                                         " +375XXXXXXXXX, или email в формате example@email.ru"
 
 
-# Тест-кейс_15
+# TC-LK.RT-15
 # Тестирование аутентификации зарегестрированного пользователя.
 def test_authorisation_valid(web_browser):
     page = AuthPage(web_browser)
