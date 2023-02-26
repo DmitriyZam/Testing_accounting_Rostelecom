f3b3b11f7dd969bc6673063409b2b49214be5e6d
ZeTol <Linkedin12332@gmail.com>
2023-02-26 20:25:57 +0300

    ок

--- autotests_rostelecom.py
+++ autotests_rostelecom.py
@@ -18,7 +18,7 @@ def test_start_page_is_correct(web_browser):
     assert page.logo_lk.get_text() == "Личный кабинет"
 
 
-# TC-LK.RT-2. fail BR001
+# TC-LK.RT-2 fail BR001
 # отображение меню входа согласно требований, симметричность 
 @pytest.mark.xfail(reason="Расположение элементов на странице не соответсвует ожидаемым требованиям")
 def test_location_of_page_blocks(web_browser):
@@ -27,7 +27,7 @@ def test_location_of_page_blocks(web_browser):
     assert page.lk_form.find(timeout=1)
 
 
-# TC-LK.RT-3. fail BR002
+# TC-LK.RT-3 fail BR002
 # проверка работы, переключения кнопки Tab меню, смена аунтефикации
 @pytest.mark.xfail(reason="Таб выбора 'Номер' не соответсвует ожидаемым требованиям")
 def test_phone_tab(web_browser):
@@ -35,7 +35,7 @@ def test_phone_tab(web_browser):
     assert page.phone_tab.get_text() == "Номер"
 
 
-# TC-LK.RT-4. fail BR003
+# TC-LK.RT-4 fail BR003
 # проверка наименования кнопки «Продолжить», согласно требованиям
 @pytest.mark.xfail(reason="Кнопка должна иметь текст 'Продолжить'")
 def test_registration_page_and_continue_button(web_browser):
@@ -52,7 +52,7 @@ def test_registration_page_and_continue_button(web_browser):
 
 
 # TC-LK.RT-5 pass
-# Регистрация пользователя с пустым полем «Имя», появления текста об ошибке
+# регистрация пользователя с пустым полем «Имя», появления текста об ошибке
 def test_registration_page_with_empty_name_field(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
@@ -68,7 +68,7 @@ def test_registration_page_with_empty_name_field(web_browser):
 
 
 # TC-LK.RT-6 pass
-# Регистрация пользователя с полем «Имя» (менее 2-х символов), появление текста об ошибке
+# регистрация пользователя с полем «Имя» (менее 2-х символов), появление текста об ошибке
 def test_registration_with_an_incorrect_value_in_the_name_field(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
@@ -84,7 +84,7 @@ def test_registration_with_an_incorrect_value_in_the_name_field(web_browser):
 
 
 # TC-LK.RT-7 pass
-# Регистрация пользователя, поле «Фамилия» (более 30-ти символов), появление текста с ошибкой
+# регистрация пользователя, поле «Фамилия» (более 30-ти символов), появление текста с ошибкой
 def test_registration_with_an_incorrect_value_in_the_last_name_field(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
@@ -115,7 +115,7 @@ def test_registration_of_an_already_registered_user(web_browser):
 
 
 # TC-LK.RT-9 fail BR004
-# Проверка наличия кнопки «х» - закрыть всплывающее окно оповещения
+# проверка наличия кнопки «х» - закрыть всплывающее окно оповещения
 @pytest.mark.xfail(reason="Должна быть кнопка закрыть 'х'")
 def test_notification_form(web_browser):
     auth_page = AuthPage(web_browser)
@@ -123,7 +123,7 @@ def test_notification_form(web_browser):
     reg_page = RegPage(web_browser, auth_page.get_current_url())
     reg_page.name_field.send_keys("Леапольд")
     reg_page.last_name_field.send_keys("Кудасов")
-    reg_page.email_or_mobile_phone_field.send_keys("+79530696975")
+    reg_page.email_or_mobile_phone_field.send_keys("+79530697386")
     reg_page.password_field.send_keys("Qwerty789")
     reg_page.password_confirmation_field.send_keys("Qwerty789")
     reg_page.continue_button.click()
@@ -133,86 +133,118 @@ def test_notification_form(web_browser):
 
 
 # TC-LK.RT-10 pass
-# Некорректный пароль при регистрации пользователя(менее 8-ми символов), появления текста с подсказкой об ошибке.
+# регистрация пользователя пароль (менее 8-ми символов), появления текста с подсказкой об ошибке
 def test_incorrect_password_during_registration(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
     reg_page = RegPage(web_browser, auth_page.get_current_url())
-    reg_page.name_field.send_keys("Алексей")
-    reg_page.last_name_field.send_keys("Константинов")
-    reg_page.email_or_mobile_phone_field.send_keys("qwerty@mail.ru")
-    reg_page.password_field.send_keys("omg87")
-    reg_page.password_confirmation_field.send_keys("omg87")
+    reg_page.name_field.send_keys("Леапольд")
+    reg_page.last_name_field.send_keys("Кудасов")
+    reg_page.email_or_mobile_phone_field.send_keys("leapoldkudasoffrus@gmail.com")
+    reg_page.password_field.send_keys("man12")
+    reg_page.password_confirmation_field.send_keys("man12")
     reg_page.continue_button.click()
     assert reg_page.error_message_password.get_text() == "Длина пароля должна быть не менее 8 символов"
 
 
 
-# TC-LK.RT-11
-# Вход по неправильному паролю в форме «Авторизация» уже зарегистрированного пользователя, надпись «Забыл пароль» перекрашивается в оранжевый цвет.
+# TC-LK.RT-11 pass
+# вход по некорректному паролю в форме «Авторизация», при повторной регистраци, появления текста с подсказкой об ошибке
 def test_authorization_of_a_user_with_an_invalid_password(web_browser):
     page = AuthPage(web_browser)
-    page.phone.send_keys('+79879746660')
-    page.password.send_keys("123qwerty")
+    page.phone.send_keys('+79530697386')
+    page.password.send_keys("Qwerty788")
     page.btn_login.click()
     assert page.message_invalid_username_or_password.get_text() == "Неверный логин или пароль"
     assert "rt-link--orange" in page.the_element_forgot_the_password.get_attribute('class')
 
 
-# TC-LK.RT-12
-# Регистрация пользователя в форме «Регистрации» в поле ввода «Фамилия» вместо кириллицы, недопустимые символы.
+# TC-LK.RT-12 pass
+# форма «Регистрации» в поле ввода «Фамилия», недопустимые символы, вместо кириллицы
 def test_instead_of_cyrillic_invalid_characters(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
     reg_page = RegPage(web_browser, auth_page.get_current_url())
-    reg_page.name_field.send_keys("Анатолий")
-    reg_page.last_name_field.send_keys(",:;;(;%")
-    reg_page.email_or_mobile_phone_field.send_keys("123@mail.ru")
-    reg_page.password_field.send_keys("12345qwerty")
-    reg_page.password_confirmation_field.send_keys("12345qwerty")
+    reg_page.name_field.send_keys("Леапольд")
+    reg_page.last_name_field.send_keys(";%:?*()"%;%")
+    reg_page.email_or_mobile_phone_field.send_keys("leapoldkudasoffrus@gmail.com")
+    reg_page.password_field.send_keys("Qwerty789")
+    reg_page.password_confirmation_field.send_keys("Qwerty789")
     reg_page.continue_button.click()
     assert reg_page.message_must_be_filled_in_cyrillic.get_text() == "Необходимо заполнить поле кириллицей. От 2 до 30 символов."
 
 
 
-# ТC-LK.RT-13
-# Поле ввода «Пароль» и поле ввода «Подтверждение пароля»  в форме «Регистрация» не совпадают.
+# ТC-LK.RT-13 pass
+# форма «Регистрация», поле ввода «Пароль» и поле ввода «Подтверждение пароля», не совпадают.
 def test_password_and_password_confirmation_do_not_match(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
     reg_page = RegPage(web_browser, auth_page.get_current_url())
-    reg_page.name_field.send_keys("Анатолий")
-    reg_page.last_name_field.send_keys("Полотно")
-    reg_page.email_or_mobile_phone_field.send_keys("123@mail.ru")
-    reg_page.password_field.send_keys("qwerty123")
-    reg_page.password_confirmation_field.send_keys("qwerty12")
+    reg_page.name_field.send_keys("Леапольд")
+    reg_page.last_name_field.send_keys("Кудасов")
+    reg_page.email_or_mobile_phone_field.send_keys("leapoldkudasoffrus@gmail.com")
+    reg_page.password_field.send_keys("Qwerty789")
+    reg_page.password_confirmation_field.send_keys("Qwerty788")
     reg_page.continue_button.click()
     assert reg_page.message_passwords_dont_match.get_text() == "Пароли не совпадают"
 
 
-# ТC-LK.RT-14
-# Не валидный email в поле ввода «Email или мобильный телефон».
+# ТC-LK.RT-14 pass
+# не валидный email в поле ввода «Email или мобильный телефон»
 def test_invalid_email_or_mobile_phone(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
     reg_page = RegPage(web_browser, auth_page.get_current_url())
-    reg_page.name_field.send_keys("Анатолий")
-    reg_page.last_name_field.send_keys("Полотно")
-    reg_page.email_or_mobile_phone_field.send_keys("112233")
-    reg_page.password_field.send_keys("qwerty")
-    reg_page.password_confirmation_field.send_keys("qwerty")
+    reg_page.name_field.send_keys("Леапольд")
+    reg_page.last_name_field.send_keys("Кудасов")
+    reg_page.email_or_mobile_phone_field.send_keys("777777")
+    reg_page.password_field.send_keys("Qwerty789")
+    reg_page.password_confirmation_field.send_keys("Qwerty789")
     reg_page.continue_button.click()
     assert reg_page.message_enter_the_phone_in_the_format.get_text() == "Введите телефон в формате +7ХХХХХХХХХХ или" \
                                                                         " +375XXXXXXXXX, или email в формате example@email.ru"
 
 
-# TC-LK.RT-15
-# Тестирование аутентификации зарегестрированного пользователя.
+# TC-LK.RT-15 pass
+# тестирование аутентификации зарегестрированного пользователя.
 def test_authorisation_valid(web_browser):
     page = AuthPage(web_browser)
-    page.phone.send_keys('+79879746660')
-    page.password.send_keys("KuL7GPYFMQMCwZd")
+    page.phone.send_keys('+79530697386')
+    page.password.send_keys("Qwerty789")
     page.btn_login.click()
 
     assert 'https://b2c.passport.rt.ru/account_b2c/page?state=' in page.get_current_url() \
            and '&client_id=account_b2c#/' in page.get_current_url()
+                                       
+                                       
+                                       
+# TC-LK.RT-16 pass
+# проверка открытия пользовательского соглашения
+def test_agreement(selenium):
+    form = AuthForm(selenium)
+
+    original_window = form.driver.current_window_handle
+    # клик по надписи "Пользовательским соглашением" в подвале страницы
+    form.agree.click()
+    sleep(5)
+    WebDriverWait(form.driver, 5).until(EC.number_of_windows_to_be(2))
+    for window_handle in form.driver.window_handles:
+        if window_handle != original_window:
+            form.driver.switch_to.window(window_handle)
+            break
+    win_title = form.driver.execute_script("return window.document.title")
+
+    assert win_title == 'User agreement' 
+                                       
+                                       
+                                       
+# TC-LK.RT-17 pass 
+# авторизации пользователя по ссылке через вконтакте
+def test_auth_vk(selenium):
+    form = AuthForm(selenium)
+    form.vk_btn.click()
+    sleep(5)
+
+    assert form.get_base_url() == 'oauth.vk.com'
+                                
